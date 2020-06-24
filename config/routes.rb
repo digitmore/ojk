0399e18dbd064d22ed3847c3d931f6c227b97f00
digitmore <64125165+digitmore@users.noreply.github.com>
2020-06-24 22:43:45 +0900

    トップページ作成＃２

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # devise_for :users
   root 'contents#index'
   resources :contents, only: [:index, :create, :edit, :update, :destroy]
   resources :users, only: [:index, :create, :edit, :update, :show, :destroy]
